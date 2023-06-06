c0c5496c83185fa625529b00422f18ba6fd8b4d7
GAKIZA <imansamgis@gmail.com>
2023-06-06 16:26:48 -0700

    5-print_comb2.py

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 
 for number in range(0, 100):
-    if number < 10:
-        print("0{}".format(number), end=", ")
+    if number == 99:
+        print("{}".format(number))
     else:
-        print("{}".format(number), end=", ")
-print()
+        print("{:02}".format(number), end=", ")
