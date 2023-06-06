6ab5d9a89526cd60bbfbcdd1717ceeaa94dd8a65
GAKIZA <imansamgis@gmail.com>
2023-06-06 15:38:16 -0700

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-#python program to print a-z
+# python program to print a-z
 
 for letter_code in range(97, 123):
-    print("{}".format(chr(letter_code)),end='')
+    print("{}".format(chr(letter_code)), end='')
