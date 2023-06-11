928f0558b80d8503164fd3895ffb60d56c918b78
GAKIZA <imansamgis@gmail.com>
2023-06-11 15:50:56 -0700

    4-new_in_list.py

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -2,7 +2,7 @@
 
 def new_in_list(my_list, idx, element):
     tmp_list = my_list[:]
-    if idx >= 0 or idx < (len(my_list)):
+    if idx >= 0 and idx < (len(my_list)):
         tmp_list[idx] = element
         return (tmp_list)
     else:
