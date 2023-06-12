39f3e3299ec2b5f8d9d63ec1fcccbf335108678c
GAKIZA <imansamgis@gmail.com>
2023-06-12 14:59:50 -0700

    9-max_integer.py

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+def max_integer(my_list):
+    if len(my_list) == 0:
+        return(None)
+
+    max_num = my_list[0]
+    for x in range(1, len(my_list)):
+        if my_list[x] > max_num:
+            max_num = my_list[x]
+
+    return (max_num)
