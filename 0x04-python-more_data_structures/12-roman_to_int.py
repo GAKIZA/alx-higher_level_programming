397fd0e9c91abf0d3a94b3d5e3c586043f52e652
GAKIZA <imansamgis@gmail.com>
2023-06-15 11:58:03 -0700

    recheck 12-

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,8 @@
 def roman_to_int(roman_string):
     if type(roman_string) != str or roman_string is None:
         return 0
-    converter = {'I': 1, 'V': 5, 'X': 10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    converter = {'I': 1, 'V': 5, 'X': 10, 'L': 50,
+                 'C': 100, 'D': 500, 'M': 1000}
     converted_number = [converter[k] for k in roman_string] + [0]
     n = 0
 
