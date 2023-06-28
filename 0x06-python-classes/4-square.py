6c10b3315cb36c25d216a8eab4b7ed1d3b379cfa
GAKIZA <imansamgis@gmail.com>
2023-06-28 04:03:12 -0700

    4-square.py

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+
+"""creating a private attribute of a class"""
+
+
+class Square:
+
+    """ Representing a square"""
+
+    def __init__(self, size=0):
+        """Initializes a new square
+
+        args:
+        size: the size of new square"""
+        self.__size = size
+
+    @property
+    def size(self):
+        """Contains the current size of a square"""
+        return (self.__size)
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ Return the current area of a square """
+        return (self.__size * self.__size)
