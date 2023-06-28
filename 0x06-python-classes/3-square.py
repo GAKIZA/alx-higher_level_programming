59e8d3c14f8fba3e8390937225926f166c3b6b0b
GAKIZA <imansamgis@gmail.com>
2023-06-27 18:49:03 -0700

    3-square.py

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""creating a private attribute of a class"""
+
+
+class Square:
+    """ Representing a square"""
+    def __init__(self, size=0):
+        """Initializes a new square
+
+        args:
+        size: the size of new square"""
+
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    def area(self):
+        """ Return the current area of a square """
+        return (self.__size * self.__size)
