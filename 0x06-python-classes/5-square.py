caddf808831a23cd31f28b4a6258dfe4ff2b9b42
GAKIZA <imansamgis@gmail.com>
2023-06-28 05:16:13 -0700

    5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -31,6 +31,7 @@ class Square:
     def area(self):
         """ Return the current area of a square """
         return (self.__size * self.__size)
+
     def my_print(self):
         """prints in stdout the square with the character #"""
         for i in range(0, self.__size):
