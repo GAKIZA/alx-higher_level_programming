526794a004c0cd31188aa79abce2aa5b35374f2d
GAKIZA <imansamgis@gmail.com>
2023-06-28 04:06:47 -0700

    4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -18,6 +18,7 @@ class Square:
     def size(self):
         """Contains the current size of a square"""
         return (self.__size)
+    
     @size.setter
     def size(self, value):
         if not isinstance(value, int):
