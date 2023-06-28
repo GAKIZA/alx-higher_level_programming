56fa344cfe8e282ea507b13e745b7524d1a30ffb
GAKIZA <imansamgis@gmail.com>
2023-06-27 18:54:32 -0700

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -4,7 +4,9 @@
 
 
 class Square:
+
     """ Representing a square"""
+
     def __init__(self, size=0):
         """Initializes a new square
 
@@ -16,6 +18,7 @@ class Square:
         elif size < 0:
             raise ValueError("size must be >= 0")
         self.__size = size
+
     def area(self):
         """ Return the current area of a square """
         return (self.__size * self.__size)
