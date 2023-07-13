a5da4f01f78d3e45b507ba73ea73699526cdbf80
GAKIZA <imansamgis@gmail.com>
2023-07-13 13:02:19 -0700

    2-is_same_class.py

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A function that determines that a given object is an instance 
+   of a given function"""
+
+def is_same_class(obj, a_class):
+    """return true if obj is an instance of a_class"""
+
+    if type(obj) == a_class:
+        return True
+    return False
