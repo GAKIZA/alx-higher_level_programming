0a08c29b9fc2890f6dacc1e2e6670455f9c934fc
GAKIZA <imansamgis@gmail.com>
2023-07-14 12:44:04 -0700

    7-base_geometry.py

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defining an empty class"""
+
+
+class BaseGeometry:
+    """Class for Base Geometry"""
+    def area(self):
+        """Area that raises an exception"""
+        raise Exception("area() is not implemented")
+    def integer_validator(self, name, value):
+        """the method that validates integers"""
+        if type(value) != int:
+            raise TypeError(f"{name} must be integer")
+        if value <= 0:
+            raise ValueError(f"{name} must be greater than zero")
