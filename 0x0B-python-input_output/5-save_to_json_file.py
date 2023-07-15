6ec7e163dbbc34113188c9dfae218302dbc89d97
GAKIZA <imansamgis@gmail.com>
2023-07-15 15:53:05 -0700

    5-save_to_json_file.py

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Function that writes an object to a text file"""
+
+import json
+
+def save_to_json_file(my_obj, filename):
+    "Writes a given object to a text file"""
+    with open(filename, 'w') as j:
+        return json.dump( my_obj, j)
