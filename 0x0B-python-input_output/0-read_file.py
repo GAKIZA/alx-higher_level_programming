ff7a10bcafa98b096b829aacec41a09a3c5ea74a
GAKIZA <imansamgis@gmail.com>
2023-07-15 13:56:02 -0700

    0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,6 @@
 
 
 def read_file(filename=""):
-"""The function that reads a file line per line"""
-    with open(filename, encoding="utf-8") as f:
-        print(f.read(), end="")
+    """The function that reads a file line per line"""
+    with open(filename, encoding="utf-8") as k:
+        print(k.read(), end="")
